0e967118174ec5156797568da93eb518f5a78844
adyatmaa86 <adyatmaa86@gmail.com>
2026-05-15 21:51:11 +0700

    perbaiki api index lagi

--- api/index.php
+++ api/index.php
@@ -1,8 +1,5 @@
 <?php
 
-// Jalankan autoload dan bootstrap Laravel dari folder root
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../bootstrap/app.php';
-
-// Jalankan aplikasi
+// Jembatan tunggal ke public/index.php
+// Laravel 11/12 akan menangani autoload dan bootstrap secara otomatis di sana.
 require __DIR__ . '/../public/index.php';
