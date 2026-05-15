50540751f0f7b3ce2423a527cc17fbeb54ca64f6
adyatmaa86 <adyatmaa86@gmail.com>
2026-05-15 21:45:27 +0700

    perbaiki index api

--- api/index.php
+++ api/index.php
@@ -1,4 +1,8 @@
 <?php
 
-// Forward Vercel requests to the Laravel index.php
+// Jalankan autoload dan bootstrap Laravel dari folder root
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../bootstrap/app.php';
+
+// Jalankan aplikasi
 require __DIR__ . '/../public/index.php';
